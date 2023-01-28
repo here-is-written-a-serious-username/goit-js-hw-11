10844f323145fcd1c69e91bf007d66d291571bdb
Lord-Bambucha <lordbambycha93@gmail.com>
2023-01-28 23:25:29 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+
+import './css/styles.css';
+// import debounce from 'lodash.debounce';
+// import { fetchCountries } from './fetchCountries';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Notify.init({
+//     timeout: 3000,
+//     position: 'center-top',
+// });
+
+
