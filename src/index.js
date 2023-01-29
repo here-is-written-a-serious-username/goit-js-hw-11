351f1164ca43fcb21259337e36ab82300e03c3d3
Lord-Bambucha <lordbambycha93@gmail.com>
2023-01-29 17:43:48 +0200

    0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 
-
-import './css/styles.css';
 // import debounce from 'lodash.debounce';
 // import { fetchCountries } from './fetchCountries';
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -10,4 +8,30 @@ import './css/styles.css';
 //     position: 'center-top',
 // });
 
+import './css/styles.css';
+const axios = require('axios').default;
 
+// console.log(axios);
+
+
+
+
+
+
+{/* <div class="photo-card">
+    <img src="" alt="" loading="lazy" />
+    <div class="info">
+        <p class="info-item">
+            <b>Likes</b>
+        </p>
+        <p class="info-item">
+            <b>Views</b>
+        </p>
+        <p class="info-item">
+            <b>Comments</b>
+        </p>
+        <p class="info-item">
+            <b>Downloads</b>
+        </p>
+    </div>
+</div> */}
