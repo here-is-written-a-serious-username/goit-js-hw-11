8339fc14c892ade70143d666cc4ce75871abe6e5
Lord-Bambucha <lordbambycha93@gmail.com>
2023-01-31 15:00:28 +0200

    131313

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const axios = require('axios').default;
 const per_page = 40;
 let page = 1;
 let newSearchQuery = null;
+let totalPages = 1;
 
 const divGallery = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
@@ -23,23 +24,24 @@ function onFormSubmit(event) {
     event.preventDefault();
     const form = event.currentTarget;
     const searchQuery = form.elements.searchQuery.value;
-    
+
     if (newSearchQuery !== searchQuery) {
         resetPage();
     }
     newSearchQuery = searchQuery;
-       
+
+    if (totalPages < page) {
+        Notify.failure('We`re sorry, but you`ve reached the end of search results.');
+        return;
+    }
+
     getPhoto(searchQuery).then(response => {
-        const totalPages = response.totalHits / per_page;
-        // console.log(totalPages);
-        // console.log(Math.ceil(totalPages));        
+        totalPages = Math.ceil(response.totalHits / per_page);
+        // console.log(totalPages); 12.5
+        // console.log(Math.ceil(totalPages)); 13    
+        console.log(page);
 
-        if (totalPages < page) {
-            Notify.failure('We`re sorry, but you`ve reached the end of search results.');
-            return;
-        }
-
-        incrementPage(); 
+        incrementPage();
 
         if (!response.hits.length) {
             Notify.failure('Sorry, there are no images matching your search query. Please try again.');
@@ -48,7 +50,7 @@ function onFormSubmit(event) {
         divGallery.innerHTML = createMarkup(response.hits);
     }).catch(error => {
         console.error(error.message);
-    }); 
+    });
 
 
 };
